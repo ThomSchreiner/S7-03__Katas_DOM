835cf6e637168bb55720fe23436daffe9996d454
Thomas Schreiner <iteano.thom@hotmail.com>
2022-07-10 20:25:46 -0300

    Resolvendo Katas

---
+++ script.js
@@ -0,0 +1,16 @@
+// Kata 1
+let main = document.querySelector("#root")
+console.log(main)
+
+// Kata 2
+main.innerText = "repetir"
+
+// Kata 3
+main.innerText = "repetir mais"
+
+// Kata 4
+let span = document.createElement("span")
+console.log(span)
+
+// Kata 5
+span.innerText = "preciso repetir"
